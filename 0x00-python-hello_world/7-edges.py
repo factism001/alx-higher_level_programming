18c0e40aaa4e143c17329439c4d1b02acf4a1229
factism001 <factism001@gmail.com>
2023-01-04 03:29:29 -0800

    selected

---
+++ 0x00-python-hello_world/7-edges.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+word = "Holberton"
+word_first_3 = word[:2]
+word_last_2 = word[7:8]
+middle_word = word[1:7]
+print("First 3 letters: {}".format(word_first_3))
+print("Last 2 letters: {}".format(word_last_2))
+print("Middle word: {}".format(middle_word))
