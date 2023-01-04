a68833854b6b31ad00439cc53aedfd85c44310fa
factism001 <factism001@gmail.com>
2023-01-04 04:15:13 -0800

    update

--- 0x00-python-hello_world/100-write.py
+++ 0x00-python-hello_world/100-write.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 import sys
-
-message = "that piece of art is useful - Dora Korpar, 2015-10-19\n"
-sys.stderr.write(message)
+sys.stderr.write("that piece of art is useful - Dora Korpar, 2015-10-19\n")
 sys.exit(1)
