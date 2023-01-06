9a653d5c60e881ce5acba17995144bd4ccc1ca06
factism001 <factism001@gmail.com>
2023-01-06 03:41:40 -0800

    args

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+
+    arg_count = len(sys.argv) - 1
+    if arg_count == 0:
+        print("{} arguments.".format(arg_count))
+    elif arg_count == 1:
+        print("{} arguments.".format(arg_count))
+    else:
+        print("{} arguments.".format(arg_count))
+
+    for i in range(arg_count):
+        print("{}: {}".format(i + 1, sys.argv[i + 1]))
