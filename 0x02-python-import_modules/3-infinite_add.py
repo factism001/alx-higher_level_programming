9cf8a784c2b14392d07242790d30b3a8f9f55479
factism001 <factism001@gmail.com>
2023-01-06 04:46:13 -0800

    update

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 if __name__  "__main__":
     import sys
-
     sum = 0
-    for i in sys.argv:
-        if i != sys.argv[0]:
-            sum += int(sum)
+    for arg in sys.argv:
+        if arg != sys.argv[0]:
+            sum += int(arg)
     print("{}".format(sum))
