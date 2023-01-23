eed0b0a073aac7c2a579ed97630835d9b9bbe957
factism001 <factism001@gmail.com>
2023-01-23 12:29:54 +0100

    print_list

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    count = 0
+    for i in range(x):
+        try:
+            print(f"{my_list[i]", end="")
+        except IndexError:
+            break
+        else:
+            count += 1
+    print()
+    return counter
