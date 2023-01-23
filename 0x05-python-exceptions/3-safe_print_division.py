66ebc5f3b8109e253abd6efe0f7e9cc53694c38f
factism001 <factism001@gmail.com>
2023-01-23 13:07:14 +0100

    update

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -2,7 +2,7 @@
 def safe_print_division(a, b):
     try:
         result = a/b
-    except ValueError:
+    except:
         result = None
     finally:
         print("Inside result: {}".format(result))
