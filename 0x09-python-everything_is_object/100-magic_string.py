db49640e99df2a9359d507a0ad251c378ca0e2bb
factism001 <factism001@gmail.com>
2023-02-01 11:13:02 +0100

    update

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-def magic_string(l=[]):
-    l += ["BestSchool"]
-    return ", ".join(l)
+def magic_string(list=[]):
+    list += [1]
+    return(("BestSchool, " * (len(list) - 1)) + "BestSchool")
