a5c89d4d6251fc76364bca29b2d49d38b88ba091
factism001 <factism001@gmail.com>
2023-02-01 10:49:31 +0100

    objects

---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Module for a class that prevents dynamic attributes creation
+"""
+
+
+class LockedClass():
+    """Class to prevent dynamic attributes creation"""
+    __slots__ = ['first_name']
+
+    def __init__(self):
+        """Init method"""
+        pass
