1b9769df575ca582c16ce3b6440348edafa53ef3
factism001 <factism001@gmail.com>
2023-02-14 12:36:52 +0100

    update

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -12,7 +12,7 @@ class Base:
 
     def __init__(self, id=None):
         """initializes the instances"""
-        if id != None:
+        if id is not None:
             self.id = id
         else:
             Base.__nb_objects += 1
