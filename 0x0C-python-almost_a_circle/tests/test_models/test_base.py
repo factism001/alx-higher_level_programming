2415ecadd2ceefef964d1937ff9b281a76cc5d8f
factism001 <factism001@gmail.com>
2023-02-14 10:37:27 +0100

    update

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -1,96 +1,96 @@
 #!/usr/bin/python3
-""" Module for test Base class """
+"""
+Unittest for base class
+"""
+
+import json
 import unittest
 from models.base import Base
-from models.square import Square
 from models.rectangle import Rectangle
-from io import StringIO
-from unittest import TestCase
-from unittest.mock import patch
+from models.square import Square
 
 
-class TestBaseMethods(unittest.TestCase):
-    """ Suite to test Base class """
+class Testbaseclass(unittest.TestCase):
+    """ test for base"""
+    def testcreateelems(self):
+        b = Base()
+        b1 = Base(76)
+        b2 = Base()
+        self.assertEqual(b.id, 1)
+        self.assertEqual(b1.id, 76)
+        self.assertEqual(b2.id, 2)
 
-    def setUp(self):
-        """ Method invoked for each test """
-        Base._Base__nb_objects = 0
+    def testsetid(self):
+        """ test for set id"""
+        b = Base("h")
+        b1 = Base({1: 2})
+        b2 = Base((1, 4))
+        b3 = Base([3, 6])
+        b4 = Base(None)
+        b5 = Base(0)
+        self.assertEqual(b.id, "h")
+        self.assertEqual(b1.id, {1: 2})
+        self.assertEqual(b2.id, (1, 4))
+        self.assertEqual(b3.id, [3, 6])
+        self.assertEqual(b4.id, 4)
+        self.assertEqual(b5.id, 0)
 
-    def test_id(self):
-        """ Test assigned id """
-        new = Base(1)
-        self.assertEqual(new.id, 1)
+#     --------- private attribute of class --------
 
-    def test_id_default(self):
-        """ Test default id """
-        new = Base()
-        self.assertEqual(new.id, 1)
-
-    def test_id_nb_objects(self):
-        """ Test nb object attribute """
-        new = Base()
-        new2 = Base()
-        new3 = Base()
-        self.assertEqual(new.id, 1)
-        self.assertEqual(new2.id, 2)
-        self.assertEqual(new3.id, 3)
-
-    def test_id_mix(self):
-        """ Test nb object attributes and assigned id """
-        new = Base()
-        new2 = Base(1024)
-        new3 = Base()
-        self.assertEqual(new.id, 1)
-        self.assertEqual(new2.id, 1024)
-        self.assertEqual(new3.id, 2)
-
-    def test_string_id(self):
-        """ Test string id """
-        new = Base('1')
-        self.assertEqual(new.id, '1')
-
-    def test_more_args_id(self):
-        """ Test passing more args to init method """
-        with self.assertRaises(TypeError):
-            new = Base(1, 1)
-
-    def test_access_private_attrs(self):
-        """ Test accessing to private attributes """
-        new = Base()
+    def testprivate(self):
+        """ test for private attribute"""
+        b = Base()
         with self.assertRaises(AttributeError):
-            new.__nb_objects
+            b.__nb_objects
 
-    def test_save_to_file_1(self):
-        """ Test JSON file """
-        Square.save_to_file(None)
-        res = "[]\n"
-        with open("Square.json", "r") as file:
-            with patch('sys.stdout', new=StringIO()) as str_out:
-                print(file.read())
-                self.assertEqual(str_out.getvalue(), res)
+#    -------------- static method to_json_string -----------
 
-        try:
-            os.remove("Square.json")
-        except:
-            pass
+    def testtojson(self):
+        """ test to json"""
+        dic = {'id': 8, 'size': 5, 'x': 6, 'y': 7}
+        stdic = json.dumps([dic])
+        self.assertEqual(Base.to_json_string([dic]), stdic)
+        r = Base.to_json_string(None)
+        self.assertEqual(r, "[]")
+        r = Base.to_json_string([])
+        self.assertEqual(r, "[]")
+        dic = [1, 2, 3]
+        r = Base.to_json_string([dic])
+        self.assertEqual(r, "[[1, 2, 3]]")
 
-        Square.save_to_file([])
-        with open("Square.json", "r") as file:
-            self.assertEqual(file.read(), "[]")
+    def testtojson1(self):
+        """error json"""
+        with self.assertRaises(TypeError):
+            Base.to_json_string()
 
-    def test_save_to_file_2(self):
-        """ Test JSON file """
-        Rectangle.save_to_file(None)
-        res = "[]\n"
-        with open("Rectangle.json", "r") as file:
-            with patch('sys.stdout', new=StringIO()) as str_out:
-                print(file.read())
-                self.assertEqual(str_out.getvalue(), res)
-        try:
-            os.remove("Rectangle.json")
-        except:
-            pass
+#   --------------  save to file -------------------------
 
-        Rectangle.save_to_file([])
-        with open("Rectangle.json", "r") as file:
-            self.assertEqual(file.read(), "[]")
+    def testsavetofile(self):
+        """ test save to file"""
+        Base.save_to_file([])
+        with open("Base.json") as fd:
+            self.assertEqual(fd.read(), "[]")
+        Base.save_to_file(None)
+        with open("Base.json") as fd:
+            self.assertEqual(fd.read(), "[]")
+
+    def testsavetofile1(self):
+        """ error save to file"""
+        with self.assertRaises(AttributeError):
+            Base.save_to_string()
+
+#    ----------------- from json --------------------------
+
+    def testfromjson(self):
+        """ test load from json"""
+        self.assertEqual(Base.from_json_string("[]"), [])
+        self.assertEqual(Base.from_json_string(None), [])
+        self.assertEqual(Base.from_json_string(""), [])
+        lista = [1, 2, 3]
+        r = Base.to_json_string(lista)
+        self.assertEqual(Base.from_json_string(r), lista)
+
+    def testfromjson1(self):
+        """ error save to file"""
+        with self.assertRaises(TypeError):
+            Base.from_json_string()
