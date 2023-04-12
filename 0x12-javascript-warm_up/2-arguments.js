2b995831f2fec7362edc5fd87c4152e8d0352e2d
factism001 <factism001@gmail.com>
2023-04-12 11:14:25 +0100

    javascript warmup

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,10 @@
+#!usr/bin/node
+if (process.argv.length === 2) {
+    console.log('No argument found');
+}
+if (process.argv.length === 3) {
+    console.log('Argument found');
+}
+if (process.argv.length > 3) {
+    console.log('Arguments found');
+}
