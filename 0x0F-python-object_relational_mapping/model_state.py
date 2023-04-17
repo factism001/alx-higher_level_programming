157278ba8efe6eb46614b0fa3309940d35ef8b5c
factism001 <factism001@gmail.com>
2023-04-17 12:41:15 +0100

    update

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -23,6 +23,6 @@ class City(Base):
     """
 
     __tablename__ = 'cities'
-    id = Column(Integer, primary_key=True, nullable=False)
+    id = Column(Integer, primary_key=True)
     name = Column(String(128), nullable=False)
     state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
