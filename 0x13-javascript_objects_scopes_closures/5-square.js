b81630750dd2454412837e39daa0cbe78b737918
factism001 <factism001@gmail.com>
2023-04-20 13:19:36 +0100

    fix

--- 0x13-javascript_objects_scopes_closures/5-square.js
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
-module.imports = class Square extends require('./4-rectangle') {
-    constructor (size) {
-        super(size, size);
-    }
+module.exports = class Square extends require('./4-rectangle') {
+  constructor (size) {
+    super(size, size);
+  }
 };
--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,10 +1,8 @@
 #!/usr/bin/node
-module.imports = class Square extends require('./5-square') {
-    charPrint(c) {
-        if (c === undefined) {
-            this.this.print();
-        } else {
-            for (let i = 0; i < this.height; i++) console.log(c.repeat(this.width));
-        }
+module.exports = class Square extends require('./5-square') {
+  charPrint (c) {
+    if (c === undefined) { this.print(); } else {
+      for (let i = 0; i < this.height; i++) { console.log(c.repeat(this.width)); }
     }
+  }
 };
