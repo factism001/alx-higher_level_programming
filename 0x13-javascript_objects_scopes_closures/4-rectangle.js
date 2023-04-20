2d12ea6fc5974c966e8760e8658037b791717f6f
factism001 <factism001@gmail.com>
2023-04-20 13:12:04 +0100

    fix

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -13,11 +13,11 @@ module.exports = class Rectangle {
     }
   }
 
-    rotate() {
-        [this.width, this.height] = [this.height, this.width];
-    }
+  rotate () {
+    [this.width, this.height] = [this.height, this.width];
+  }
 
-    double() {
-        [this.width, this.height] = [this.width * 2, this.height * 2];
-    }
+  double () {
+    [this.width, this.height] = [this.width * 2, this.height * 2];
+  }
 };
