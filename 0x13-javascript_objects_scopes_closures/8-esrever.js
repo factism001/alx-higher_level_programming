22a0c3f1a37af5143c571187f38c64937949334b
factism001 <factism001@gmail.com>
2023-04-20 13:24:35 +0100

    fix

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,15 +1,8 @@
 #!/usr/bin/node
-exports.nbOccurences = function (list, searchElement) {
-  let count = 0;
-  list.map(
-    (x) => {
-      if (x === searchElement) {
-        count += 1;
-        return 1;
-      } else {
-        return 0;
-      }
-    }
-  );
-  return count;
+exports.esrever = function (list) {
+  return list.reduceRight(function (array, current) {
+    array.push(current);
+    return array;
+  },
+  []);
 };
--- 0x13-javascript_objects_scopes_closures/9-logme.js
+++ 0x13-javascript_objects_scopes_closures/9-logme.js
@@ -2,4 +2,4 @@
 let count = 0;
 exports.logMe = function (item) {
   console.log(`${count++}: ${item}`);
-}
+};
