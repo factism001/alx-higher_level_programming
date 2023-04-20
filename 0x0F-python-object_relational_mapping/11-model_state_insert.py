1dd0a83201b8cc46f3c9b1e47a154b58d4b62964
factism001 <factism001@gmail.com>
2023-04-20 12:39:18 +0100

    fix

--- 0x0F-python-object_relational_mapping/11-model_state_insert.py
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -3,17 +3,20 @@
  adds the State object "Louisiana" to the database hbtn_0e_6_usa
 """
 
+import sys
+from unicodedata import name
+from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from model_state import Base, State
+from model_state import State
 
-if __name__ == '__main__':
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
-        sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping+True)
+if __name__ == "__main__":
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+    session_maker = sessionmaker(bind=engine)
+    session = session_maker()
 
-    Session = sessionmaker(bind=engine)
-    session = Session()
-
-    new_state = State(name='Louisiana')
-    session.add(new_state)
+    obj = State(name="Louisiana")
+    session.add(obj)
     session.commit()
-    print(new_state.id)
+    print(obj.id)
