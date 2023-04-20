dc20667a140050cc681f2ed1052ff165073f28e0
factism001 <factism001@gmail.com>
2023-04-20 12:12:08 +0100

    fix

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,19 +1,20 @@
 #!/usr/bin/python3
-""" takes in an argument and displays all values
-    in the states table of hbtn_0e_0_usa
-    where name matches the argument
+""" takes in an argument
+    and displays all values
+    in the states table of
+    hbtn_0e_0_usa where name matches the argument
      Usage: ./2-my_filter_states.py <mysql username>
                                     <mysql password>
                                     <database name>
                                     <state name searched>
 """
-
 import sys
 import MySQLdb
 
 if __name__ == "__main__":
     db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
-    cur = db.cursor()
-    cur.execute("SELECT * FROM state ORDER BY id \
-                 where BINARY name = '{}'".format(sys.argv[4]))
-    [print(state) for state in cur.fetchall()]
+    c = db.cursor()
+    c.execute("""SELECT * FROM states
+                WHERE name LIKE BINARY '{}'
+                ORDER BY states.id ASC""".format(sys.argv[4]).strip("'"))
+    [print(state) for state in c.fetchall()]
